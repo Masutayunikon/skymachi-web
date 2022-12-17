83498f10021beebf9d7844581bfecd764074b2d9
Quentin Robert <71890754+Masutayunikon@users.noreply.github.com>
2022-12-17 22:01:45 +0400

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="container">
+    <Renderer ref="renderer" resize="true" orbit-ctrl>
+      <Camera ref="camera" :position="{ z: 100 }" :lookAt="{x: 100, y: 100, z: 100}"/>
+      <Scene background="white">
+        <AmbientLight color="#ffffff" :intensity="1"/>
+        <Box ref="box" :position="{x: 0, y:0, z:0}">
+          <ObsidianTexture />
+        </Box>
+        <Box ref="box" :position="{x: 0, y:1, z:0}">
+          <ObsidianTexture />
+        </Box>
+        <Box ref="box" :position="{x: 0, y:2, z:0}">
+          <ObsidianTexture />
+        </Box>
+        <Box ref="box" :position="{x: 0, y:3, z:0}">
+          <ObsidianTexture />
+        </Box>
+        <Box ref="box" :position="{x: 0, y:3, z:0}">
+          <ObsidianTexture />
+        </Box>
+        <Box ref="box" :position="{x: 0, y:4, z:0}">
+          <ObsidianTexture />
+        </Box>
+        <Box ref="box" :position="{x: 1, y:4, z:0}">
+          <ObsidianTexture />
+        </Box>
+        <Box ref="box" :position="{x: 2, y:4, z:0}">
+          <ObsidianTexture />
+        </Box>
+        <Box ref="box" :position="{x: 3, y:4, z:0}">
+          <ObsidianTexture />
+        </Box>
+        <Box ref="box" :position="{x: 3, y:3, z:0}">
+          <ObsidianTexture />
+        </Box>
+        <Box ref="box" :position="{x: 3, y:2, z:0}">
+          <ObsidianTexture />
+        </Box>
+        <Box ref="box" :position="{x: 3, y:1, z:0}">
+          <ObsidianTexture />
+        </Box>
+        <Box ref="box" :position="{x: 3, y:0, z:0}">
+          <ObsidianTexture />
+        </Box>
+        <Box ref="box" :position="{x: 2, y:0, z:0}">
+          <ObsidianTexture />
+        </Box>
+        <Box ref="box" :position="{x: 1, y:0, z:0}">
+          <ObsidianTexture />
+        </Box>
+      </Scene>
+    </Renderer>
+  </div>
+</template>
+
+<script setup>
+import { Renderer, Camera, Scene, PointLight, Box, LambertMaterial, AmbientLight, BasicMaterial, Texture, CubeTexture } from 'troisjs'
+import ObsidianTexture from "./components/ObsidianTexture";
+
+const renderer = ref(null)
+const box = ref(null)
+const camera = ref(null)
+
+</script>
+
+<style lang="scss">
+
+html {
+  box-sizing: border-box;
+}
+
+*, *::before, *::after{
+  box-sizing: inherit;
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  background-color: red;
+  width: 100vw;
+  height: 100vh;
+}
+
+</style>
---
+++ components/ObsidianTexture.vue
@@ -0,0 +1,23 @@
+<template>
+  <BasicMaterial>
+    <Texture ref="obsidianTexture" refraction :refraction-ratio="0.95" src="/block/obsidian.png" />
+  </BasicMaterial>
+</template>
+
+<script setup>
+import { BasicMaterial, Texture } from 'troisjs'
+import * as THREE from "three";
+
+const obsidianTexture = ref(null)
+
+onMounted(() => {
+  const t = obsidianTexture.value
+  t.texture.magFilter = THREE.NearestFilter;
+  t.texture.minFilter = THREE.NearestFilter;
+})
+
+</script>
+
+<style scoped>
+
+</style>
